a0aa7934a2116c20861e723b843b2e236b45d6b7
Nerv3sine <44034888+Nerv3sine@users.noreply.github.com>
2022-02-01 00:13:13 -0800

    init push foundational a5 program required by the assignment

---
+++ files/a5.cpp
@@ -0,0 +1,40 @@
+// a5.cpp
+
+/////////////////////////////////////////////////////////////////////////
+//
+// Student Info
+// ------------
+//
+// Name : Jason Cai
+// St.# : 301426943
+// Email: jason_cai_4@sfu.ca
+//
+//
+// Statement of Originality
+// ------------------------
+//
+// All the code and comments below are my own original work. For any non-
+// original work, I have provided citations in the comments with enough
+// detail so that someone can see the exact source and extent of the
+// borrowed work.
+//
+// In addition, I have not shared this work with anyone else, and I have
+// not seen solutions from other students, tutors, websites, books,
+// etc.
+//
+/////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+
+#include "Menu.h"
+#include "Database.h"
+#include "Operator.h"
+
+using namespace std;
+
+int main(){
+    cout << "running program...\n";
+    Menu* mainProgram = new Menu();
+    mainProgram->startUp();
+    delete mainProgram;
+}
